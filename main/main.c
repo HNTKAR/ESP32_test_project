ad092ddb7fa56efd270e1b2f85cb3ee1c0f8e34b
kusari-k <45356209+kusari-k@users.noreply.github.com>
2021-02-20 21:58:47 +0900

    Lチカ(GPIO2)

--- main/main.c
+++ main/main.c
@@ -11,27 +11,28 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
-
+#include "driver/gpio.h"
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    printf("Lチカ!!!\n");
+    gpio_config_t PINA;
+    PINA.pin_bit_mask = (1ULL << 2);
+    PINA.mode = GPIO_MODE_OUTPUT;
+    PINA.pull_up_en = false;
+    PINA.pull_down_en = false;
+    PINA.intr_type = GPIO_INTR_DISABLE;
+    gpio_config(&PINA);
 
     /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    for (int i = 10; i >= 0; i--) {
+    for (int i = 100; i >= 0; i--)
+    {
         printf("Restarting in %d seconds...\n", i);
+        if (i % 2)
+            gpio_set_level(GPIO_NUM_2, 1);
+        else
+            gpio_set_level(GPIO_NUM_2, 0);
+
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
     printf("Restarting now.\n");
